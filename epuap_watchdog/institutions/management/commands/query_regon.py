c974c011d5490e39db4c1347ac7d4f2ae01d81e0
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-08-17 12:52:40 +0200

    Query REGON throught Google by NIP

--- epuap_watchdog/institutions/management/commands/query_regon.py
+++ epuap_watchdog/institutions/management/commands/query_regon.py
@@ -12,6 +12,7 @@ from tqdm import tqdm
 from epuap_watchdog.institutions.models import REGON
 
 REGON_PATTERN = re.compile('([0-9]{9,14}|[0-9]{9}-[0-9]{5})')
+NIP_PATTERN = re.compile('([0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}|[0-9]{3}-[0-9]{2}-[0-9]{2}-[0-9]{3}|[0-9]{11})')
 
 
 class Command(BaseCommand):
@@ -24,8 +25,10 @@ class Command(BaseCommand):
         parser.add_argument('--google', type=str, nargs='+', help="Use Google to guest REGON number")
         parser.add_argument('--comment', required=True, help="Description of changes eg. data source description")
         parser.add_argument('--no-progress', dest='no_progress', action='store_false')
+        parser.add_argument('--no-regon', dest='no_regon', action='store_true')
+        parser.add_argument('--no-nip', dest='no_nip', action='store_true')
 
-    def handle(self, krs, regon, nip, comment, google, no_progress, *args, **options):
+    def handle(self, krs, regon, nip, comment, google, no_progress, no_regon, no_nip, *args, **options):
         gus = GUS(api_key=settings.GUSREGON_API_KEY, sandbox=settings.GUSREGON_SANDBOX)
         if settings.GUSREGON_SANDBOX is True:
             self.stderr.write("You are using sandbox mode for the REGON database. Data may be incorrect. "
@@ -35,7 +38,7 @@ class Command(BaseCommand):
         if not any([krs, regon, nip, google]):
             raise CommandError("Provide at least one '--krs' or '--regon' or '--nip' or '--google' is required. ")
 
-        queries = self.get_queryset(krs, nip, regon, google)
+        queries = self.get_queryset(krs, nip, regon, google, no_regon, no_nip)
 
         with transaction.atomic() and reversion.create_revision():
             reversion.set_comment(comment)
@@ -67,26 +70,36 @@ class Command(BaseCommand):
                            "{} inserted and "
                            "{} errored.").format(total, self.updated, self.skipped, self.inserted, self.errored))
 
-    def get_queryset(self, krs, nip, regon, google):
+    def get_queryset(self, krs, nip, regon, google, no_regon, no_nip):
         regon = regon or []
+        nip = nip or []
 
-        processor = html2text.HTML2Text()
-        processor.ignore_emphasis = True
-        processor.bypass_tables = True
-        processor.ignore_links = True
+        self.processor = html2text.HTML2Text()
+        self.processor.ignore_emphasis = True
+        self.processor.bypass_tables = True
+        self.processor.ignore_links = True
 
-        session = requests.Session()
-        for keyword in google:
-            content = session.get('https://www.google.pl/search', params={'q': "{} REGON".format(keyword)}).text
-            text = processor.handle(content)
-            result = REGON_PATTERN.findall(text)
-            print("For '{}' found {}".format(keyword, result))
-            regon += result
+        self.session = requests.Session()
+        for keyword in tqdm(google):
+            if not no_regon:
+                result = self.search_google("{} REGON".format(keyword), REGON_PATTERN)
+                print("For '{}' found {}".format(keyword, result))
+                regon += result
+            if not no_nip:
+                result = self.search_google("{} NIP".format(keyword), NIP_PATTERN)
+                print("For '{}' found {}".format(keyword, result))
+                nip += [x.replace('-', '') for x in result if len(x.replace('-', '')) == 10]
         queries = [{'krs': v} for v in set(krs)] if krs else []
         queries += [{'nip': v} for v in set(nip)] if nip else []
         queries += [{'regon': v} for v in set(regon)] if regon else []
 
         return queries
+
+    def search_google(self, query, pattern):
+        content = self.session.get('https://www.google.pl/search', params={'q': query}).text
+        text = self.processor.handle(content)
+        result = pattern.findall(text)
+        return result
 
     def get_iter(self, queryset, no_progress):
         return tqdm(queryset, smoothing=0) if no_progress else queryset
